29a26f8f8639467248591f7616e62ea6a19086f1
philiphester <69607375+philiphester@users.noreply.github.com>
2025-08-16 17:58:18 -0400

    Add comments for getComputerChoice function

--- script.js
+++ script.js
@@ -4,8 +4,10 @@
 // Randomly return Rock, Paper, or Scissors and return answer
 function getComputerChoice() {
     let computerChoice;
+    // Get random number between 0 and 2
     let randomNum = Math.floor(Math.random() * 3);
 
+    // Set computerchoice based on the random number set to variable randomNum
     if (randomNum === 0) {
         computerChoice = "Rock";
     } else if (randomNum === 1) {
